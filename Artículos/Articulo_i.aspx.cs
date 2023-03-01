f46cb3b34e1566dc67cb229c36cf585adfe89119
unknown <panipatsurvivor@gmail.com>
2023-02-28 23:09:13 -0600

    Insert Store Procedure commited

---
+++ Artículos/Articulo_i.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Artículos
+{
+    public partial class Articulo_i : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-38G492P;Initial Catalog=Prueba1;Integrated Security=True");
+        protected void Button2_Click(object sender, EventArgs e)//boton insertar
+        {
+            string nombreArticulo = TextBox1.Text;
+            double precioArticulo = double.Parse(TextBox2.Text);
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = conexion;
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "SP_Insert1";
+
+            command.Parameters.AddWithValue("ParNombre", nombreArticulo);
+            command.Parameters.AddWithValue("ParPrecio", precioArticulo);
+
+            conexion.Open();
+            command.ExecuteNonQuery();
+            conexion.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Insercion exitosa');", true);
+
+            /*
+            //Se asignan las variables para los atributos a insertar
+            string articulo = TextBox1.Text;
+            double precio = double.Parse(TextBox2.Text);
+            conexion.Open();//Se abre la conexion
+            SqlCommand command = new SqlCommand("Insert into Articulo values ('" + articulo + "','" + precio + "')", conexion);
+            command.ExecuteNonQuery();
+            conexion.Close();//Se cierra la conexion
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Insercion exitosa');", true);*/
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)//boton cerrar
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
---
+++ Artículos/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace Artículos
+{
+
+    public partial class _Default : Page
+    {
+        SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-38G492P;Initial Catalog=Prueba1;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.GridView1.Attributes.Add("bordercolor","#000");
+            this.GetListaDeArticulos();
+        }
+
+        protected void GetListaDeArticulos()
+        {
+            SqlCommand command = new SqlCommand("EXECUTE ShowData1", conexion);
+            SqlDataAdapter sd = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)//boton cerrar
+        {
+            System.Environment.Exit(0);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)//boton insertar
+        {
+            Response.Redirect("Articulo_i.aspx");
+        }
+
+    }
+
+}
